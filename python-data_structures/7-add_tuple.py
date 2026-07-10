8a792a36868b6b0e1edaf3d8fd7058dacb94fbcd
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-10 08:16:29 -0700

    Add 7-add_tuple.py

---
+++ python-data_structures/7-add_tuple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Pad both tuples with (0, 0) and slice the first two elements
+    a = (tuple_a + (0, 0))[:2]
+    b = (tuple_b + (0, 0))[:2]
+    
+    # Return the sum of the respective elements as a new tuple
+    return (a[0] + b[0], a[1] + b[1])
