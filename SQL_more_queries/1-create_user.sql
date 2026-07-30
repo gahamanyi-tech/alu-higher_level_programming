93763eaae6b0f86aa012cb7ad64c4c4805cac464
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-30 10:10:54 -0700

    Update 1-create_user.sql: use mysql_native_password

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,5 +1,5 @@
 -- Creates the user user_0d_1 if it doesn't already exist,
--- sets password, and grants all privileges
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+-- sets password (mysql_native_password), and grants all privileges
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
