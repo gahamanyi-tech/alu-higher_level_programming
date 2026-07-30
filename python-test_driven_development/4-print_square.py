c4df86a353678cbf1a09fe7d206954c3c631fe9d
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-30 15:59:54 -0700

    Update 4-print_square.py and doctests to cover missing argument and PEP8 checks

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python3
-"""Module that provides a function to print a square with # characters."""
+"""
+This module provides a function `print_square` that prints a square
+using the `#` character.
+"""
 
 
 def print_square(size):
-    """Prints a square with the character # of size `size`."""
+    """
+    Prints a square with the character #.
+
+    Args:
+        size: Length of the square side.
+
+    Raises:
+        TypeError: If size is not an integer or is a float < 0.
+        ValueError: If size is < 0.
+    """
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if not isinstance(size, int):
