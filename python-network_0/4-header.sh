a244c34732bc87923506dd930dc960a30194c8d0
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-30 12:36:48 -0700

    Add explicit GET method flag to 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header variable
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to URL with custom header variable
+curl -sX GET -H "X-HolbertonSchool-User-Id: 98" "$1"
