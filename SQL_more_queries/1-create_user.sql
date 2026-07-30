76a2235ea80e47e3db720fbd71ac08429963f770
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-30 06:34:13 -0700

    Add 1-create_user.sql: create user_0d_1 with all privileges

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,5 @@
--- Creates the MySQL server user user_0d_1 with all privileges
+-- Creates the user user_0d_1 if it doesn't already exist,
+-- sets password, and grants all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
+FLUSH PRIVILEGES;
