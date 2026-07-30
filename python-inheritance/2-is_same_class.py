3ea6d9cbd9db5fe2c9511c9f7aa890dc070ceab5
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-30 05:03:27 -0700

    Fix PEP8 E501 line length in 2-is_same_class.py

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,5 +6,7 @@ Checks exact class instance matching.
 
 
 def is_same_class(obj, a_class):
-    """Returns True if obj is exactly an instance of a_class; otherwise False."""
+    """Returns True if obj is exactly an instance of a_class;
+    otherwise False.
+    """
     return type(obj) is a_class
