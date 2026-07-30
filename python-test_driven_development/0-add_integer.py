617f2c0913c80f8147184bbb77444d76e3e6215c
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-30 15:49:19 -0700

    Fix PEP 8 line-length and formatting issues in 0-add_integer.py

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -23,9 +23,11 @@ def add_integer(a, b=98):
     if type(b) not in (int, float):
         raise TypeError("b must be an integer")
 
-    if isinstance(a, float) and (a != a or a == float('inf') or a == float('-inf')):
+    if isinstance(a, float) and (a != a or a == float('inf') or
+                                 a == float('-inf')):
         raise TypeError("a must be an integer")
-    if isinstance(b, float) and (b != b or b == float('inf') or b == float('-inf')):
+    if isinstance(b, float) and (b != b or b == float('inf') or
+                                 b == float('-inf')):
         raise TypeError("b must be an integer")
 
     return int(a) + int(b)
