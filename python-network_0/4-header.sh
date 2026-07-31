d70a0c0d713a8372304fc0b81165095dcffa3644
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-31 14:41:40 -0700

    Fix curl header flag in 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header and displays the response body
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to a URL with a header variable X-HolbertonSchool-User-Id: 98
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
