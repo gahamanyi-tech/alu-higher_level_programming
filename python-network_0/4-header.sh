317fba4fc1227825585334ca00cf8b6c15064b5c
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-31 14:53:23 -0700

    Fix header flag separation and formatting in 4-header.sh

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to a URL with a custom header and displays the body
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request with a specific header parameter to a URL
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
