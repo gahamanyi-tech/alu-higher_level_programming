fc56e90da4d98de917df69f75c229de42c3d1882
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-31 14:43:04 -0700

    Update 4-header.sh header flag syntax

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL with a header variable X-HolbertonSchool-User-Id: 98
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -s --header "X-HolbertonSchool-User-Id: 98" "$1"
