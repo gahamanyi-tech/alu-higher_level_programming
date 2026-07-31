6feffc921240a55f7f98cc5b0cd9bc8a7be7f475
gahamanyi-tech <g.gahamanyi@alustudent.com>
2026-07-31 15:21:55 -0700

    Fix 4-header.sh: preserve curl stderr output for checker validation

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Sends a GET request to a URL with the X-HolbertonSchool-User-Id header and displays the response body
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+curl -H "X-HolbertonSchool-User-Id: 98" "$1"
